65bb8c4207426f6fac905f9c45e290ae224c81c0
Peter Fritz <61599784+peterfritz@users.noreply.github.com>
2020-04-14 21:00:58 -0300

    added license headers

--- PreciseStopwatch.bat
+++ PreciseStopwatch.bat
@@ -1,3 +1,9 @@
+:: Copyright (c) 2018-2020 Peter Fritz
+::
+:: This Source Code Form is subject to the terms of the Mozilla Public
+:: License, v. 2.0. If a copy of the MPL was not distributed with this
+:: file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 @echo off
 title PreciseStopwatch by Peter Fritz
 mode con: cols=80 lines=10
